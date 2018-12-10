5a2bde21961a55971efef9a7f1b51f448767f57b
Andrew Sutton <asutton@lock3software.com>
2018-12-10 13:04:05 -0500

    2018-07-30  Andrew Sutton  <andrew.n.sutton@gmail.com>
    	Import tests from concepts TS tests.
    	* g++.dg/cpp2a/concepts2.C: New tests.

--- gcc/testsuite/g++.dg/cpp2a/concepts2.C
+++ gcc/testsuite/g++.dg/cpp2a/concepts2.C
@@ -4,10 +4,15 @@
 template<typename T>
 concept Class = __is_class(T);
 
+template<typename T>
+concept EmptyClass = Class<T> && __is_empty(T);
+
 template<typename T, typename U>
 concept Classes = __is_class(T) && __is_class (U);
 
 struct empty { };
+
+struct nonempty { int n; };
 
 template<typename T>
 struct S
@@ -29,7 +34,7 @@ struct S
 
 struct X { };
 
-int main()
+void driver_1()
 {
   S<X> s1;
   s1.f(); // OK
@@ -51,19 +56,48 @@ int main()
 
 template<typename T>
   requires Class<T>
-void constrained_f(T x) { }
+void f1(T x) { }
 
+// fn1.C
 template<typename T>
 void caller_1(T x) { 
-  constrained_f(x); // Unchecked dependent arg.
+  f1(x); // Unchecked dependent arg.
 }
 
 template<typename T>
 void caller_2(T x) { 
-  constrained_f(empty{}); // Checked non-dependent arg, but OK
+  f1(empty{}); // Checked non-dependent arg, but OK
 }
 
+// fn2.C
 template<typename T>
 void caller_3() {
-  constrained_f(0); // { dg-error "cannot call function" }
+  f1(0); // { dg-error "cannot call function" }
+}
+
+// fn3.c
+
+int called = 0;
+
+template<typename T> 
+  requires Class<T> 
+constexpr int f2(T x) { return 1; }
+
+template<typename T> 
+  requires EmptyClass<T> 
+constexpr int f2(T x) { return 2; }
+
+template<typename T> 
+constexpr int f3(T x) requires Class<T> { return 1; }
+
+template<typename T> 
+constexpr int f3(T x) requires EmptyClass<T> { return 2; }
+
+void driver_2() {
+  f2(0); // { dg-error "no matching function" }
+  static_assert (f2(empty{}) == 2);
+  static_assert (f2(nonempty{}) == 1);
+  f3(0); // { dg-error "no matching function" }
+  static_assert (f3(empty{}) == 2);
+  static_assert (f3(nonempty{}) == 1);
 }
