bd75f1f56779d5baa79211ed45c035cd584ff605
Andrew Sutton <asutton@lock3software.com>
2018-12-10 15:41:55 -0500

    2018-09-11  Andrew Sutton  <andrew.n.sutton@gmail.com>
    	Moving tests around.
    	* g++.dg/concepts/req1.C: Deleted.
    	* g++.dg/concepts/req2.C: Deleted.
    	* g++.dg/concepts/req3.C: Deleted.
    	* g++.dg/concepts/req4.C: Deleted.
    	* g++.dg/concepts/req5.C: Deleted.
    	* g++.dg/concepts/req6.C: Deleted.
    	* g++.dg/concepts/req7.C: Deleted.
    	* g++.dg/cpp2a/concepts-requires3.C: Added tests.

--- gcc/testsuite/g++.dg/cpp2a/concepts-requires3.C
+++ gcc/testsuite/g++.dg/cpp2a/concepts-requires3.C
@@ -1,16 +1,29 @@
 // { dg-do compile }
 // { dg-options "-std=c++2a" }
 
+// req8.C
+// This is just a sanity test for type requirements.
+
+template<typename T>
+concept Has_member_type = requires { typename T::type; };
+
+template<typename T>
+concept Concept = true && Has_member_type<T>;
+
+template<typename T>
+  requires Concept<T>
+void foo(T t) { }
+
 // req9.C
 
 template<typename T>
-struct S1 {};
+struct S1 { };
 
 template<typename T>
 concept C = requires(T x) { { x.fn() } -> S1<T>; };
 
 template<typename U>
-requires C<U>
+  requires C<U>
 void fn(U x)
 {
   x.fn();
